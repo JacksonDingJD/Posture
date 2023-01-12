960b8b19cc799420cfc07293e633665337ec7ae7
jambompeople <jambompeople@gmail.com>
2023-01-11 22:39:26 -0500

    Crescent School Logo Added
    Crescent School Logo Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {useState} from "react";
 import "./App.css";
+import crest from './crescent-school.png';
 
 function App() {
 
   return (
     <div>
       <div className="center-flex background-color">
-        <h1 id="Title"> PoseTure </h1>{" "}
+        <h1 id="Title"> Crescent Outreach </h1>
+        <img src={crest} alt="rightCrest" />
       </div>
       
     </div>
